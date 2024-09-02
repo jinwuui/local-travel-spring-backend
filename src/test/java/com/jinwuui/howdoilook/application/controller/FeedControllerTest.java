64642ecd7ccacf9cea62003115081b7d967edefe
jinwuui <jinwuui@gmail.com>
2024-09-02 12:08:53 +0900

    feat: add request domain for creating feed

--- src/test/java/com/jinwuui/howdoilook/application/controller/FeedControllerTest.java
+++ src/test/java/com/jinwuui/howdoilook/application/controller/FeedControllerTest.java
@@ -1,5 +1,6 @@
-package com.jinwuui.howdoilook.controller;
+package com.jinwuui.howdoilook.application.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@Slf4j
 @WebMvcTest
 class FeedControllerTest {
 
