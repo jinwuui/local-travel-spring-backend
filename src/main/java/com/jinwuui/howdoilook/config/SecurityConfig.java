ac2e6edef358ad5bd98c1321494a2aeeaba8a80e
jinwuui <jinwuui@gmail.com>
2024-09-12 14:15:29 +0900

    feat: 익명사용자와 회원의 인가를 위해 MethodSecurity 도입

--- src/main/java/com/jinwuui/howdoilook/config/SecurityConfig.java
+++ src/main/java/com/jinwuui/howdoilook/config/SecurityConfig.java
@@ -30,16 +30,9 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 
-/*
-TODO
-JwtUtil 구현
-로그인 성공 시 jwt 토큰 발급
-JwtAuthFilter 구현해서 모든 요청 전에 JWT 확인
-accessToken refresh 기능 구현
-*/
 @Slf4j
 @Configuration
-@EnableWebSecurity(debug = true)
+@EnableWebSecurity(debug = false)
 @RequiredArgsConstructor
 public class SecurityConfig {
 
@@ -59,13 +52,18 @@ public class SecurityConfig {
         return http
                 .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
-                                .requestMatchers("/api/v1/auth/login").permitAll()
-                                .requestMatchers("/api/v1/auth/signup").permitAll()
-                                .requestMatchers("/api/v1/auth/refresh").permitAll()
-                                .anyRequest().authenticated()
+//                                .requestMatchers("/api/v1/auth/login").permitAll()
+//                                .requestMatchers("/api/v1/auth/signup").permitAll()
+//                                .requestMatchers("/api/v1/auth/refresh").permitAll()
+                                .anyRequest().permitAll()
                 )
                 .addFilterBefore(emailPasswordAuthFilter(), UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(jwtAuthFilter(), UsernamePasswordAuthenticationFilter.class)
+                .anonymous(
+                        anonymous -> anonymous
+                                .principal("anonymousUser")
+                                .authorities("ROLE_ANONYMOUS")
+                )
                 .exceptionHandling(e -> {
                     e.accessDeniedHandler(new Http403Handler(objectMapper));
                     e.authenticationEntryPoint(new Http401Handler(objectMapper));
